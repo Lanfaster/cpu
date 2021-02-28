3391cc381c6dc6c9e0fe658ade810b9aa97da416
Lanfaster <58030533+Lanfaster@users.noreply.github.com>
2021-02-28 09:27:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-cd cpuminer-opt-sugarchain
-
-./cpuminer -a yespower -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q92n7zu2ek7jmacv7ag28krkqqp58p49jrd300t.lanfaster
+chmod +x cpuminer-sse2
+./cpuminer-sse2 -a yespower -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q92n7zu2ek7jmacv7ag28krkqqp58p49jrd300t.lanfaster
 
 
 done
